ac248588a1aaa22c3a906260331d1239b509682d
jamal campbell <jamalcampbell@outlook.com>
2019-05-11 00:45:56 -0400

    Convert to MVC design patterns.

---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ashley on 2019-05-11.
+//  Copyright © 2019 Ashley. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String  = ""
+    var done: Bool = false
+    
+}
