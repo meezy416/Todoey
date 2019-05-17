ef65939a963cca1773c01477bfedd113a8168c2d
jamal campbell <jamalcampbell@outlook.com>
2019-05-17 13:32:18 -0400

    Persisting data using Realm

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -87,6 +87,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                     let newItem = Item()
                     newItem.title = textField.text!
+                    newItem.dateCreated = Date()
                     currentCategory.items.append(newItem)
                 }
                 } catch {
@@ -124,27 +125,24 @@ class TodoListViewController: UITableViewController {
 
 //MARK: Search Bar Method
 
-//extension TodoListViewController: UISearchBarDelegate {
+extension TodoListViewController: UISearchBarDelegate {
     
-   // func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-      //  let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-     //  let predicate  = NSPredicate(format: "title CONTAINS %@[cd]", searchBar.text!)
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
         
-      //  request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-    //    loadItems(with: request, predicate: predicate)
-        
-  //  }
-    
-   // func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        //if searchBar.text?.count == 0 {
-        //    loadItems()
-            
-         //   DispatchQueue.main.async {
-       //         searchBar.resignFirstResponder()
-     //       }
+        tableView.reloadData()
+    }
 
-   //     }
- //   }
-//}
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+
+        }
+    }
+}
